abc1e8fb93869e50bbc7b9b8d1e55ec470fe8fb3
robby <robby.remmenga@gmail.com>
2019-10-22 11:51:38 -0400

    changes

--- src/app.js
+++ src/app.js
@@ -1,25 +1,5 @@
+const {
+  default: { singleDeckGame }
+} = require('blackjack-dealer-logic');
 
 
-
-const buttons = document.querySelectorAll(".button");
-
-const backgroundButton = document.querySelector(".addBackground");
-
-buttons.forEach(function(button) {
-  button.addEventListener("click", () => {
-    const paragraph = document.createElement("p");
-
-    paragraph.classList.add("newParagraph");
-
-    paragraph.textContent = "My new paragraph!";
-
-    document.body.append(paragraph);
-  });
-});
-
-backgroundButton.addEventListener("click", () => {
-  const paragraph = document.querySelector(".newParagraph");
-
-  paragraph.classList.add("redBackground");
-  paragraph.classList.remove("newParagraph");
-});
